35180650e2590603de8ea55f5268f763ed885a46
Christopher Jones <cjones999983@gmail.com>
2018-06-10 01:22:26 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -16,7 +16,13 @@ class Student
   end
 
   def self.count_all_students_in_grade_9
+    sql = <<-SQL
+      SELECT * FROM students
+      WHERE grade = 9;
+    SQL
 
+    DB[:conn].execute(sql)
+    binding.pry
   end
 
   def self.find_by_name(name)
