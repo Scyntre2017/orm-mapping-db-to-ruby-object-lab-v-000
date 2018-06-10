ff3f1629b6f66bfb30bcf41b2b3c7e4389de4234
Christopher Jones <cjones999983@gmail.com>
2018-06-10 01:41:11 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -39,6 +39,17 @@ class Student
     below_grade_12.map { |row| self.new_from_db(row) }
   end
 
+  def self.first_X_students_in_grade_10(amount)
+    sql = <<-SQL
+      SELECT * FROM students
+      WHERE grade = 10
+      LIMIT ?;
+    SQL
+
+    x_students = DB[:conn].execute(sql, amount)
+    x_students.map { |row| self.new_from_db(row) }
+  end
+
   def self.find_by_name(name)
     sql = <<-SQL
       SELECT * FROM students
